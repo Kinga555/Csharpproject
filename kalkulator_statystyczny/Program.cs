18db50201cb150324a1239969ace6fe199f7d446
Kinga555 <kingazszymkiewicz@microsoft.wesi .edu.pl>
2020-07-23 21:31:52 +0200

    wprowadzenie danych Program.cs

--- kalkulator_statystyczny/Program.cs
+++ kalkulator_statystyczny/Program.cs
@@ -6,7 +6,64 @@ namespace kalkulator_statystyczny
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double b;
+            bool c;
+
+            Console.WriteLine("Czy dane podane są w szeregu rozdzielczym? (T)");
+            string kindData = Console.ReadLine();
+            Console.WriteLine("Podaj ilośc obserwacji");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wprowadź szereg liczebności odzielając dane spacją");
+
+            string[] x = Console.ReadLine().Split(' ');
+
+            double[] num = new double[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                c = double.TryParse(x[i], out b);
+                if (c)
+                {
+                    num[i] = b;
+                }
+                else
+                {
+                    do
+                    {
+
+                        Console.WriteLine($"Dana na pozycji {i} niezgodna z formatem\n" +
+                            $"Ponownie wczytaj daną");
+                        c = double.TryParse(Console.ReadLine(), out b);
+                    } while (c != true);
+                }
+                num[i] = b;
+            }
+
+            Console.WriteLine("Wprowadź szereg  danej x");
+            string[] y = Console.ReadLine().Split(' ');
+
+            double[] dsx = new double[y.Length];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                c = double.TryParse(x[i], out b);
+                if (c != true)
+                {
+
+                    do
+                    {
+
+                        Console.WriteLine($"Dana na pozycji {i} niezgodna z formatem\n" +
+                            $"Ponownie wczytaj daną");
+                        c = double.TryParse(Console.ReadLine(), out b);
+                    } while (c != true);
+                    num[i] = b;
+                }
+            }
+
+
+
+
         }
     }
 }
