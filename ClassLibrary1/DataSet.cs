9e2227bdd2b9fbd1837b3c3ceddf3c24be482742
Kinga555 <kingazszymkiewicz@microsoft.wesi .edu.pl>
2020-07-24 23:57:14 +0200

    zmiana DataSet

--- ClassLibrary1/DataSet.cs
+++ ClassLibrary1/DataSet.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Data;
 
 namespace ClassLibrary1
 {
     public class DataSet
     {
-        public List<Data> DataSetList { get; private set; }
+        protected List<Data> datas;
+        public List<Data> GetDataList => datas;
         private double[] tabNumer { get; set; }
         private double[] tabStartRange { get; set; }
         private double[] tabEndOfRange { get; set; }
 
-        public DataSet()
+        public DataSet(double[] num, double[] start)
         {
-            DataSetList = new List<Data>();
+            datas = new List<Data>();
+            tabNumer = num;
+            tabStartRange = start;
             SetUpSimpleDataList();
         }
+        public DataSet(double[] num, double[] start, double[] end)
+        {
+            datas = new List<Data>();
+            tabNumer = num;
+            tabStartRange = start;
+            tabEndOfRange = end;
+            SetUpRangeDataList();
+        }
+
         public void SetUpSimpleDataList()
         {
             for (int i = 0; i < tabNumer.Length; i++)
             {
                 Data data = new Data(tabNumer[i], tabStartRange[i]);
-                DataSetList.Add(data);
+                datas.Add(data);
             }
 
         }
@@ -29,9 +40,11 @@ namespace ClassLibrary1
         {
             for (int i = 0; i < tabNumer.Length; i++)
             {
-                Data data = new Data(tabNumer[i], tabStartRange[i] - tabEndOfRange[i]);
+                Data data = new Data(tabNumer[i], (tabStartRange[i] - tabEndOfRange[i]) / 2);
+                datas.Add(data);
             }
         }
+
 
     }
 }
