29424573478f71db62ee79aca6eb44d3a029f8f8
Kinga555 <kingazszymkiewicz@microsoft.wesi .edu.pl>
2020-07-31 15:28:38 +0200

    ostateczna wersja

--- ClassLibrary1/DataSet.cs
+++ ClassLibrary1/DataSet.cs
@@ -3,80 +3,107 @@ using System.Data;
 
 namespace ClassLibrary1
 {
+    /// <summary>
+    /// Klasa  implementująca dane wprowadzone w konsoli
+    /// </summary>
     public class DataSet
     {
         protected List<Data> datas;
+        /// <summary>
+        /// zwraca dane jako listę w formacie (liczebność, dana x)
+        /// </summary>
         public List<Data> GetDataList => datas;
-        private double[] tabNumer { get; set; }
-        private double[] tabStartRange { get; set; }
-        private double[] tabEndOfRange { get; set; }
+        public double[] TabNumer { get; private set; }
+        public double[] TabStartRange { get; private set; }
+        public double[] TabEndOfRange { get; private set; }
 
+        //konstruktor dla szeregu x bez szeregu liczebnosci
         public DataSet(double[] start)
         {
             datas = new List<Data>();
-            SetUpRangeDataList();
-        }
 
+            TabStartRange = start;
+
+            SetUpDataList();
+        }
+        //konstruktor dla danych z szeregiem liczebnosci i szeregiem szczegółowym danej x
         public DataSet(double[] num, double[] start)
         {
             datas = new List<Data>();
-            tabNumer = num;
-            tabStartRange = start;
+            TabNumer = num;
+            TabStartRange = start;
             SetUpSimpleDataList();
         }
+        //konstruktor dla danych z szeregiem liczebności i szeregiem rozdzielczym x
         public DataSet(double[] num, double[] start, double[] end)
         {
             datas = new List<Data>();
-            tabNumer = num;
-            tabStartRange = start;
-            tabEndOfRange = end;
+            TabNumer = num;
+            TabStartRange = start;
+            TabEndOfRange = end;
             SetUpRangeDataList();
         }
-        public void SetUpDataList()
+
+        private void SetUpDataList()
         {
-            for (int i = 1; i <= tabNumer.Length; i++)
+            for (int i = 0; i < TabStartRange.Length; i++)
             {
-                Data data = new Data(i, tabStartRange[i]);
+                Data data = new Data(i + 1, TabStartRange[i]);
                 datas.Add(data);
             }
         }
 
-        public void SetUpSimpleDataList()
+        private void SetUpSimpleDataList()
         {
-            for (int i = 0; i < tabNumer.Length; i++)
+            for (int i = 0; i < TabStartRange.Length; i++)
             {
-                Data data = new Data(tabNumer[i], tabStartRange[i]);
+                Data data = new Data(TabNumer[i], TabStartRange[i]);
                 datas.Add(data);
             }
 
         }
-        public void SetUpRangeDataList()
+        private void SetUpRangeDataList()
         {
-            for (int i = 0; i < tabNumer.Length; i++)
+
+            for (int i = 0; i < TabStartRange.Length; i++)
             {
-                Data data = new Data(tabNumer[i], (tabStartRange[i] - tabEndOfRange[i]) / 2);
+                double v = ((TabEndOfRange[i] - TabStartRange[i]) / 2) + TabStartRange[i];
+                Data data = new Data(TabNumer[i], dataX: v);
                 datas.Add(data);
             }
         }
+        /// <summary>
+        /// metoda wylicza średnią arytmetyczną
+        /// </summary>
+        /// <param name="datas"></param>
+        /// <returns> srednia arytmetyczna</returns>
         public double Averange(List<Data> datas)
         {
 
             double sum = 0;
-            for (int i = 1; i < datas.Count; i++)
+            for (int i = 0; i < datas.Count; i++)
             {
                 sum += datas[i].DataX;
             }
             return sum / datas.Count;
         }
-        public double AverangeW(List<Data> list)
+        /// <summary>
+        /// metoda wylicza średnią ważoną
+        /// </summary>
+        /// <param name="datas"></param>
+        /// <returns>średnia ważona</returns>
+        public double AverangeW(List<Data> datas)
         {
             double sum = 0;
-            for (int i = 1; i < list.Count; i++)
+            double f = 0;
+            for (int i = 0; i < datas.Count; i++)
             {
-                sum += list[i].Numer * list[i].DataX;
+                sum += datas[i].Numer * datas[i].DataX;
+                f += datas[i].Numer;
             }
-            return sum / list.Count;
+            return sum / f;
         }
+
 
 
     }
--- kalkulator_statystyczny/Program.cs
+++ kalkulator_statystyczny/Program.cs
@@ -9,28 +9,23 @@ namespace kalkulator_statystyczny
         {
             double b;
             bool c;
-
-
-
-            Console.WriteLine("Czy dane podane są w szeregu rozdzielczym? (t/n)");
-            string kindData = Console.ReadLine();
             Console.WriteLine("Podaj ilośc obserwacji");
             int length = int.Parse(Console.ReadLine());
             double[] num = new double[length];
             double[] dsx = new double[length];
             double[] dex = new double[length];
+            Console.WriteLine("Czy dane podane są w szeregu rozdzielczym? (t/n)");
+            string kindData = Console.ReadLine();
+
             Console.WriteLine("czy twoje dane zawierają szereg liczebności? (t/n)");
             string ifNumer = Console.ReadLine();
             if (ifNumer == "t")
             {
-                Console.WriteLine("Wprowadź szereg liczebności odzielając dane spacją");
-
-                string[] x = Console.ReadLine().Split(' ');
-
+                Console.WriteLine("Wprowadź szereg liczebności ");
 
                 for (int i = 0; i < length; i++)
                 {
-                    c = double.TryParse(x[i], out b);
+                    c = double.TryParse(Console.ReadLine(), out b);
                     if (c)
                     {
                         num[i] = b;
@@ -48,15 +43,17 @@ namespace kalkulator_statystyczny
                 }
             }
 
-            Console.WriteLine("Wprowadź szereg  danej x");
-            string[] y = Console.ReadLine().Split(' ');
-
-
+            Console.WriteLine("Wprowadź szereg  danej x ( lub początek przedziału  x)");
 
             for (int i = 0; i < length; i++)
             {
-                c = double.TryParse(y[i], out b);
-                if (c != true)
+                c = double.TryParse(Console.ReadLine(), out b);
+                if (c)
+                {
+
+                    dsx[i] = b;
+                }
+                else
                 {
 
                     do
@@ -67,23 +64,25 @@ namespace kalkulator_statystyczny
                     } while (c != true);
                     dsx[i] = b;
                 }
+
             }
             if (kindData == "t")
             {
                 Console.WriteLine("Wprowadź szereg końca przedziału danej x");
-                string[] z = Console.ReadLine().Split(' ');
-
-
 
                 for (int i = 0; i < length; i++)
                 {
-                    c = double.TryParse(z[i], out b);
-                    if (c != true)
+                    c = double.TryParse(Console.ReadLine(), out b);
+                    if (c)
                     {
 
+                        dex[i] = b;
+                    }
+                    else
+                    {
                         do
                         {
-                            Console.WriteLine($"Dana na pozycji {i} niezgodna z formatem\n" +
+                            Console.WriteLine($"Dana na pozycji {i + 1} niezgodna z formatem\n" +
                                 $"Ponownie wczytaj daną");
                             c = double.TryParse(Console.ReadLine(), out b);
                         } while (c != true);
@@ -92,23 +91,31 @@ namespace kalkulator_statystyczny
                 }
             }
 
-            if (ifNumer == "t" || kindData != "t")
+
+            if (ifNumer == "t" && kindData != "t")
             {
                 DataSet ds = new DataSet(num, dsx);
-                double sred = ds.Averange(ds.GetDataList);
-                Console.WriteLine($"Sredna arytmetyczna{ sred}");
+
+                Console.WriteLine($"Sredna arytmetyczna x: {ds.Averange(ds.GetDataList)}");
+                Console.WriteLine($" Srednia ważona: {ds.AverangeW(ds.GetDataList)}");
+
             }
-            else if (ifNumer != "t" || kindData == "t")
+            else if (ifNumer != "t" && kindData != "t")
             {
-                DataSet ds = new DataSet(num, dsx);
-                double sred = ds.AverangeW(ds.GetDataList);
-                Console.WriteLine($"Sredna ważona { sred}");
+                DataSet ds = new DataSet(dsx);
+
+                Console.WriteLine($"Sredna arytmetyczna { ds.Averange(ds.GetDataList)}");
+            }
+            else if (ifNumer == "t" && kindData == "t")
+            {
+                DataSet ds = new DataSet(num, dsx, dex);
+                Console.WriteLine($"Sredna arytmetyczna x ( wg środka przedziału): {ds.Averange(ds.GetDataList)}");
+                Console.WriteLine($"Srednia ważona: {ds.AverangeW(ds.GetDataList)}");
             }
             else
             {
                 DataSet ds = new DataSet(num, dsx, dex);
-                double sred = ds.AverangeW(ds.GetDataList);
-                Console.WriteLine($"Srednia ważona: {sred}");
+                Console.WriteLine($"Sredna arytmetyczna { ds.Averange(ds.GetDataList)}");
             }
 
 
